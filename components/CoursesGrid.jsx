8d486fb1fb3734d9c910d476b0dc1d13af504d63
Adnan Lahrech <lahadn@gmail.com>
2021-12-19 14:05:32 +0100

    courses grid

---
+++ components/CoursesGrid.jsx
@@ -0,0 +1,41 @@
+import {Box, Grid, Col, Pagination, Center, LoadingOverlay} from '@mantine/core'
+import CourseCard from './CourseCard'
+import {useState} from 'react'
+import useSWR from 'swr'
+
+export default function CoursesShowcase({api, pagination, size, ...restProps}) {
+  const [activePage, setPage] = useState(1)
+  const {data, error} = useSWR(`${api}?page=${activePage}&limit=${size}`)
+
+  if (error) return console.log('error is ', error)
+  if (error) return <Center>فشل في التحميل</Center>
+
+  if (data) console.log('data', data)
+
+  const setChange = page => {
+    setPage(page)
+  }
+
+  const CoursesList = data?.map((course, idx) => {
+    return (
+      <Col key={`${idx}-${course.title}`} span={3}>
+        <CourseCard course={course} />
+      </Col>
+    )
+  })
+
+  return (
+    <Box {...restProps}>
+      <LoadingOverlay visible={!data} />
+      <Box>
+        <Grid columns={12}>{CoursesList}</Grid>
+      </Box>
+
+      {pagination && (
+        <Center mt="md">
+          <Pagination total={5} page={activePage} onChange={setChange} />
+        </Center>
+      )}
+    </Box>
+  )
+}
